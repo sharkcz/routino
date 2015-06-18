32c31138099a0e29f47ce9bbfc3bba1700155340
amb <amb@cdd41072-61d7-4102-955e-fb406ba21da9>
2015-06-18 18:54:16 +0000

    Add stub files for Routino library exported functions and Makefile support.
    git-svn-id: http://routino.org/svn/branches/libroutino@1715 cdd41072-61d7-4102-955e-fb406ba21da9

---
+++ src/routino.c
@@ -0,0 +1,26 @@
+/***************************************
+ Routino library functions file.
+
+ Part of the Routino routing software.
+ ******************/ /******************
+ This file Copyright 2015 Andrew M. Bishop
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************/
+
+#include "routino.h"
+
+DLL_PUBLIC void Routino(void)
+{
+}
---
+++ src/routino.h
@@ -0,0 +1,74 @@
+/***************************************
+ Routino library header file.
+
+ Part of the Routino routing software.
+ ******************/ /******************
+ This file Copyright 2015 Andrew M. Bishop
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************/
+
+
+#ifndef ROUTINO_H
+#define ROUTINO_H    /*+ To stop multiple inclusions. +*/
+
+/* Limit the exported symbols in the library */
+
+#if __GNUC__ >= 4
+  #ifdef LIBROUTINO
+    #define DLL_PUBLIC __attribute__ ((visibility ("default")))
+  #endif
+#endif
+
+#if defined(_MSC_VER)
+  #ifdef LIBROUTINO
+    #define DLL_PUBLIC __declspec(dllexport)
+  #else
+    #define DLL_PUBLIC __declspec(dllimport)
+  #endif
+#endif
+
+#if defined(__MINGW32__) || defined(__CYGWIN__)
+  #ifdef LIBROUTINO
+    #define DLL_PUBLIC __attribute__ ((dllexport))
+  #else
+    #define DLL_PUBLIC __attribute__ ((dllimport))
+  #endif
+#endif
+
+#ifndef DLL_PUBLIC
+  #define DLL_PUBLIC
+#endif
+
+
+/* Handle compilation with a C++ compiler */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+ /* Library functions */
+
+ DLL_PUBLIC void Routino(void);
+
+
+/* Handle compilation with a C++ compiler */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ROUTINO_H */
