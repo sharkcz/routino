sharkcz/routino

commit 629e70d1dde9e155ce951c4132aa3cfd7e265054

If the number of items to be sorted is small then do not create multiple threads.
git-svn-id: http://routino.org/svn/trunk@2138 cdd41072-61d7-4102-955e-fb406ba21da9

src/sorting.c
@@ -142,7 +142,9 @@ index_t filesort_fixed(int fd_in,int fd_out,size_t itemsize,int (*pre_sort_funct
  if(nitems==0)
     return(0);
 
- if((nitems*(itemsize+sizeof(void*)))<option_filesort_ramsize)
+ if((nitems*(itemsize+sizeof(void*)))<(option_filesort_ramsize/option_filesort_threads))
+    /* use one thread */;
+ else if((nitems*(itemsize+sizeof(void*)))<option_filesort_ramsize)
     nitems=1+nitems/option_filesort_threads;
  else
     nitems=option_filesort_ramsize/(option_filesort_threads*(itemsize+sizeof(void*)));
@@ -542,7 +544,9 @@ index_t filesort_vary(int fd_in,int fd_out,int (*pre_sort_function)(void*,index_
     one will require RAM for data, FILESORT_VARALIGN and sizeof(void*)
     Assume that data+FILESORT_VARALIGN+sizeof(void*) is 4*data. */
 
- if((datasize*4)<option_filesort_ramsize)
+ if((datasize*4)<option_filesort_ramsize/option_filesort_threads)
+    /* use one thread */;
+ else if((datasize*4)<option_filesort_ramsize)
     datasize=(datasize*4)/option_filesort_threads;
  else
     datasize=option_filesort_ramsize/option_filesort_threads;

commit 0d6b06894f1afcd7b60b402b3049a2f346a7777b

Fix a bug that was introduced in r2138 (sorting failure in some cases, unclear which but not found in tests of slim mode or when using multiple threads).
git-svn-id: http://routino.org/svn/trunk@2157 cdd41072-61d7-4102-955e-fb406ba21da9

src/sorting.c
@@ -545,7 +545,8 @@ index_t filesort_vary(int fd_in,int fd_out,int (*pre_sort_function)(void*,index_
     Assume that data+FILESORT_VARALIGN+sizeof(void*) is 4*data. */
 
  if((datasize*4)<option_filesort_ramsize/option_filesort_threads)
-    /* use one thread */;
+    /* use one thread */
+    datasize=datasize*4;
  else if((datasize*4)<option_filesort_ramsize)
     datasize=(datasize*4)/option_filesort_threads;
  else